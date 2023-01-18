f4b44f5575783b00c882230ba51049a478a32bab
Sahil Kumar Gupta <g.sahil@iitg.ac.in>
2023-01-18 12:03:02 +0530

    tp

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
         <ul>
           <li>
             <a href='/home'>Home Page</a>{" "}
-            <a href='/events-page'>Events Page</a>{" "}
+            <a href='/scheduke'>Schedule Page</a>{" "}
           </li>
         </ul>
       </div>
