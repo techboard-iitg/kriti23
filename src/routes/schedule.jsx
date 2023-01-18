f0fdb8936cfb78e8697a68c9ff13360a69c8db23
Sahil Kumar Gupta <g.sahil@iitg.ac.in>
2023-01-18 19:07:20 +0530

    events page

--- src/routes/schedule.jsx
+++ src/routes/schedule.jsx
@@ -1,12 +1,38 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client";
+import moment from 'moment'
 
 
 export default function Schedule() {
+
+  const [schedule, setSchedule] = useState();
+
+    useEffect(() => {
+		sanityClient
+			.fetch(
+				`*[_type == "events"]{
+                    event_name,
+                    event_date,
+                    event_cup,
+                    event_points,
+                }`
+			)
+			.then((data) => setSchedule(data))
+			.catch(console.error);
+	}, []);
+  function compare( a, b ) {
+      if ( a.event_date < b.event_date){
+        return -1;
+      }
+      if ( a.event_date > b.event_date){
+        return 1;
+      }
+      return 0;
+  }
     return (
         <div style={{margin:"3%"}}>
-            <h1>hiii</h1>
-            <br/><br/>
+            <h1 className="text-3xl customBlue-300 font-bold">Event Dates - KRITI (2023) :-</h1>
+            <br/>
             <table style={{borderStyle: "solid", borderColor: "#032538", borderWidth: 1, borderRadius: 1}} className="min-w-full">
               <thead style={{backgroundColor: "#032538", color: "#EFEFEF"}}>
                 <tr>
@@ -23,21 +49,21 @@ export default function Schedule() {
                     Points
                   </th>
                 </tr>
+                
               </thead>
               <tbody  className="divide-y divide-customBlue-200">
-                <tr>
-                  <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">General Quiz</td>
-                  <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">12th February</td>
-                  <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">Brainiac</td>
-                  <td className="py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">200</td>
-                </tr>
 
-                <tr>
-                  <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">General Quiz</td>
-                  <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">12th February</td>
-                  <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">Brainiac</td>
-                  <td className="py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">200</td>
-                </tr>
+                {schedule && 
+                 schedule.sort(compare) && 
+                 schedule.map( (ps_event, ind) => (
+                    <tr style={{ backgroundColor: (ind % 2 == 0) ? '#FDFEFF' : '#EFEFEF' }}>
+                      <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">{ps_event.event_name}</td>
+                      <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">{moment(ps_event.event_date).format("Do MMM YY")}</td>
+                      <td className="px-6 py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">{ps_event.event_cup}</td>
+                      <td className="py-4 text-base font-medium text-customBlue-300 whitespace-nowrap">{ps_event.event_points}</td>
+                    </tr>                  
+                 ))
+                }
                 
               </tbody>
             </table>
