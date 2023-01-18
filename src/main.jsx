29b11c1fc5cb41e3c138c2d654dda4c31993d44b
Aman Kumar <aman.26262@gmail.com>
2023-01-18 11:40:09 +0530

    tailwind and router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className='text-blue-500'>
+        <ul>
+          <li>
+            <a href='/home'>Home Page</a>{" "}
+          </li>
+        </ul>
+      </div>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
