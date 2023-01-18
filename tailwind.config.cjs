0301ef6b49e98d6f502420fd83fbfb77adb0ec41
Aman Kumar <aman.26262@gmail.com>
2023-01-18 17:58:38 +0530

    colors fixed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,13 +5,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      tableCellBorder: '#4A6CB2',
-      tableBorder: '#032538',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        tableCellBorder: '#4A6CB2',
+        tableBorder: '#032538',
+        customBlue: {
+          100: "#EDF8FE",
+          200: "#4A6CB2",
+          300: "#032538",
+        }
+      },
     },
+    
   },
   plugins: [],
 }
