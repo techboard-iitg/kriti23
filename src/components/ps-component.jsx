22dc64bffbc97fd6ba01b71c949306fc5c586a68
Sahil Kumar Gupta <g.sahil@iitg.ac.in>
2023-01-24 00:01:45 +0530

    link_newtab

--- src/components/ps-component.jsx
+++ src/components/ps-component.jsx
@@ -7,7 +7,7 @@ const PSComponent = ({ title, date, link }) => {
       <div className='flex justify-between items-center'>
         <div>Date: {date}</div>
         <button className='bg-customBlue-300 text-white px-4 py-2 rounded'>
-          <a href={link}>View More</a>
+          <a href={link} target="_blank">View More</a>
         </button>
       </div>
     </div>
